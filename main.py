7b5974b2c4ee6f836ef810d94c6e1c4356116fe5
p32 <ostik19791108@gmail.com>
2023-05-07 14:49:12 +0300

    e21w

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hi Charlota")
+a = (input(":"))
+b = str(input(":"))
+list = [a,b]
+my_dict = {
+    "Anna" :1969 ,
+    "name" : "Nick",
+    "City" : "lviv"
+}
+
+
+for x in my_dict.values():
+    if x in list:
+        print("yes")
+    else:
+        print("no")
